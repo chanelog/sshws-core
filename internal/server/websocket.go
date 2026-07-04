356e135d08c7ac52346c9a00722c3f4c560df073
chanelog <chanelog93@gmail.com>
2026-07-04 14:33:51 +0700

    Update websocket.go

--- internal/server/websocket.go
+++ internal/server/websocket.go
@@ -17,23 +17,33 @@ var upgrader = websocket.Upgrader{
 
 func (s *Server) handleWebSocket(w http.ResponseWriter, r *http.Request) {
 
-	conn, err := upgrader.Upgrade(w, r, nil)
+	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		logger.Error.Println(err)
 		return
 	}
-	defer conn.Close()
+	defer ws.Close()
 
-	logger.Info.Println("New WebSocket:", r.RemoteAddr)
+	logger.Info.Printf("New client: %s\n", r.RemoteAddr)
 
-	for {
-		mt, msg, err := conn.ReadMessage()
-		if err != nil {
-			break
-		}
-
-		if err := conn.WriteMessage(mt, msg); err != nil {
-			break
-		}
+	tcp, err := dialBackend(s.cfg.Backend)
+	if err != nil {
+		logger.Error.Println(err)
+		return
 	}
+	defer tcp.Close()
+
+	done := make(chan struct{}, 2)
+
+	go func() {
+		copyTCPToWS(ws, tcp)
+		done <- struct{}{}
+	}()
+
+	go func() {
+		copyWSToTCP(ws, tcp)
+		done <- struct{}{}
+	}()
+
+	<-done
 }
