1abf303008f8c4f217f3c39873272122a54fa942
chanelog <chanelog93@gmail.com>
2026-07-04 14:13:54 +0700

    Update websocket.go

--- internal/server/websocket.go
+++ internal/server/websocket.go
@@ -3,6 +3,7 @@ package server
 import (
 	"net/http"
 
+	"github.com/chanelog/sshws-core/internal/logger"
 	"github.com/gorilla/websocket"
 )
 
@@ -12,4 +13,27 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
+}
+
+func (s *Server) handleWebSocket(w http.ResponseWriter, r *http.Request) {
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		logger.Error.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	logger.Info.Println("New WebSocket:", r.RemoteAddr)
+
+	for {
+		mt, msg, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+
+		if err := conn.WriteMessage(mt, msg); err != nil {
+			break
+		}
+	}
 }
