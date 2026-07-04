fe53b4906a254b81c1a97ea1d6a97abbfafa4f8f
chanelog <chanelog93@gmail.com>
2026-07-04 14:05:18 +0700

    Update server.go

--- internal/server/server.go
+++ internal/server/server.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/chanelog/sshws-core/internal/config"
 	"github.com/chanelog/sshws-core/internal/logger"
-	"github.com/gorilla/websocket"
 )
 
 type Server struct {
@@ -19,53 +18,25 @@ func New(cfg *config.Config) *Server {
 	}
 }
 
-func (s *Server) routes(http.HandleFunc(s.cfg.Path, s.handleWebSocket) {
+func (s *Server) routes() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
 		fmt.Fprintln(w, "SSHWS Core Running")
-
 	})
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-
 		fmt.Fprintln(w, "OK")
-
 	})
 
 	http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
-
 		fmt.Fprintln(w, "SSHWS Core v1.0.0-dev")
-
 	})
 
+	http.HandleFunc(s.cfg.Path, s.handleWebSocket)
 }
 
 func (s *Server) Start() error {
-func (s *Server) handleWebSocket(w http.ResponseWriter, r *http.Request) {
 
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		logger.Error.Println(err)
-		return
-	}
-
-	defer conn.Close()
-
-	logger.Info.Println("New WebSocket:", r.RemoteAddr)
-
-	for {
-
-		mt, msg, err := conn.ReadMessage()
-		if err != nil {
-			break
-		}
-
-		if err := conn.WriteMessage(mt, msg); err != nil {
-			break
-		}
-	}
-}
 	s.routes()
 
 	logger.Info.Println("===================================")
@@ -77,5 +48,4 @@ func (s *Server) handleWebSocket(w http.ResponseWriter, r *http.Request) {
 		s.cfg.Listen,
 		nil,
 	)
-
 }
